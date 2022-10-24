41e90e2ad78bbcd70f255397b76ab10bd044f238
Emmanuel <emmanuellmhango@gmail.com>
2022-10-25 02:53:06 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,27 +4,27 @@ const booksSection = document.querySelector('.books-section');
 const addButton = document.querySelector('.add-button');
 let library = [];
 
-let data = JSON.parse(localStorage.getItem('libraryData'));
+const data = JSON.parse(localStorage.getItem('libraryData'));
 
 if (data !== null) {
-  for (let i=0; i<data.length; i+=1) {
+  for (let i = 0; i < data.length; i += 1) {
     const newBook = document.createElement('div');
     newBook.classList.add('book-info');
     booksSection.appendChild(newBook);
 
     const titleSpan = document.createElement('span');
-    titleSpan.textContent = data[i]['title'];
+    titleSpan.textContent = data[i].title;
     newBook.appendChild(titleSpan);
   
     const authorSpan = document.createElement('span');
-    authorSpan.textContent = data[i]['author'];
+    authorSpan.textContent = data[i].author;
     newBook.appendChild(authorSpan);
-  
+
     const removeButton = document.createElement('button');
     removeButton.classList.add('remove-button');
     removeButton.textContent = 'Remove';
     newBook.appendChild(removeButton);
-  
+
     newBook.appendChild(document.createElement('hr'));
 
     library.push({title: data[i]['title'], author: data[i]['author']});
