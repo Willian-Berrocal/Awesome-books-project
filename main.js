1c3269d93eb54b57d8ad8cfd971b14844e0b9784
Willian-Berrocal <willianberrocal@yahoo.com>
2022-10-24 17:22:18 -0500

    Remove functionality done

--- main.js
+++ main.js
@@ -2,49 +2,58 @@ const authorInput = document.querySelector('.form-author');
 const titleInput = document.querySelector('.form-title');
 const booksSection = document.querySelector('.books-section');
 const addButton = document.querySelector('.add-button');
-
+let library = [];
 
 let data = JSON.parse(localStorage.getItem('libraryData'));
 
 if (data !== null) {
   for (let i=0; i<data.length; i+=1) {
+    const newBook = document.createElement('div');
+    newBook.classList.add('book-info');
+    booksSection.appendChild(newBook);
+
     const titleSpan = document.createElement('span');
     titleSpan.textContent = data[i]['title'];
-    booksSection.appendChild(titleSpan);
+    newBook.appendChild(titleSpan);
   
     const authorSpan = document.createElement('span');
     authorSpan.textContent = data[i]['author'];
-    booksSection.appendChild(authorSpan);
+    newBook.appendChild(authorSpan);
   
     const removeButton = document.createElement('button');
     removeButton.classList.add('remove-button');
     removeButton.textContent = 'Remove';
-    booksSection.appendChild(removeButton);
+    newBook.appendChild(removeButton);
   
-    booksSection.appendChild(document.createElement('hr'));  
+    newBook.appendChild(document.createElement('hr'));
+
+    library.push({title: data[i]['title'], author: data[i]['author']});
   }
+  localStorage.setItem('libraryData', JSON.stringify(library));
 }
 
-
-const library = [];
 
 function AddBook (e) {
   e.preventDefault();
 
+  const newBook = document.createElement('div');
+  newBook.classList.add('book-info');
+  booksSection.appendChild(newBook);
+
   const titleSpan = document.createElement('span');
   titleSpan.textContent = titleInput.value;
-  booksSection.appendChild(titleSpan);
+  newBook.appendChild(titleSpan);
 
   const authorSpan = document.createElement('span');
   authorSpan.textContent = authorInput.value;
-  booksSection.appendChild(authorSpan);
+  newBook.appendChild(authorSpan);
 
   const removeButton = document.createElement('button');
   removeButton.classList.add('remove-button');
   removeButton.textContent = 'Remove';
-  booksSection.appendChild(removeButton);
+  newBook.appendChild(removeButton);
 
-  booksSection.appendChild(document.createElement('hr'));
+  newBook.appendChild(document.createElement('hr'));
 
   library.push({title:titleInput.value, author:authorInput.value});
   localStorage.setItem('libraryData', JSON.stringify(library));
@@ -52,3 +61,18 @@ function AddBook (e) {
 
 addButton.addEventListener('click', AddBook);
 
+
+const removeButton = document.querySelectorAll('.remove-button');
+
+function removeBook (e) {
+  e.preventDefault();
+  const parentDiv = e.currentTarget.parentNode;
+  parentDiv.remove();
+
+  const bookTitle = e.currentTarget.previousElementSibling.previousElementSibling.textContent;
+
+  library = library.filter(obj => obj.title!==bookTitle);
+  localStorage.setItem('libraryData', JSON.stringify(library));
+}
+
+removeButton.forEach((btn) => btn.addEventListener('click', removeBook));
