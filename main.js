b224099bc5ec9322dabb72bf464259f410353fa5
Emmanuel <emmanuellmhango@gmail.com>
2022-10-26 01:57:35 +0400

    Update main.js

--- main.js
+++ main.js
@@ -6,29 +6,22 @@ btn.addEventListener('click', () => {
   const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
   const myBook = new MyBooks(bookTitle, bookAuthor);
+  
   if (myBook.addBook() === true) {
-    booksDiv.innerHTML += `
-    <p id="${books.length - 1}-del" class="book-class">
-        <span>"${bookTitle}" by ${bookAuthor}</span>
-        <button class="delete-button" id="${books.length - 1}">Remove</button>
-    </p>`;
-    history.go(0);
-  } else {
-    alert('an error occured, please try again later1');
-  }
+    myBook.previewBook(booksDiv, books.length - 1);
+    window.history.go(0);
+  } 
 });
 
-myBooks.displayBooks();
+MyBooks.displayBooks(booksDiv);
 
 const del = document.querySelectorAll('.delete-button');
 del.forEach((element) => {
   element.addEventListener('click', () => {
     const id = parseInt(element.getAttribute('id'), 10);
-    if (myBooks.removeBook(id) === true){
+    if (MyBooks.removeBook(id) === true){
       const y = id.toString().concat('-del');
       document.getElementById(y).remove();
-    } else {
-      alert('an error occured, please try again later3');
     } 
   });
 });
