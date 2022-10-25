2fd26ee2ea75e77e30519310a260997954087bde
Emmanuel <emmanuellmhango@gmail.com>
2022-10-26 02:02:38 +0400

    Update index.html

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ btn.addEventListener('click', () => {
   const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
   const myBook = new MyBooks(bookTitle, bookAuthor);
-  
   if (myBook.addBook() === true) {
     myBook.previewBook(booksDiv, books.length - 1);
     window.history.go(0);
-  } 
+  }
 });
 
 MyBooks.displayBooks(booksDiv);
@@ -19,9 +18,9 @@ const del = document.querySelectorAll('.delete-button');
 del.forEach((element) => {
   element.addEventListener('click', () => {
     const id = parseInt(element.getAttribute('id'), 10);
-    if (MyBooks.removeBook(id) === true){
+    if (MyBooks.removeBook(id) === true) {
       const y = id.toString().concat('-del');
       document.getElementById(y).remove();
-    } 
+    }
   });
 });
