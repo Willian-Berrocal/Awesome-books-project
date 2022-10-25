8cf589577d5c0fa3baafd5e168f0c4d4f5c79cfa
Emmanuel <emmanuellmhango@gmail.com>
2022-10-26 01:54:02 +0400

    Update js_classes.js

--- js_classes.js
+++ js_classes.js
@@ -1,19 +1,21 @@
 class MyBooks {
   constructor(title, author) {
-      this.title = title;
-      this.author = author;
-      MyBooks.val = true;
+    this.title = title;
+    this.author = author;
+    MyBooks.val = true;
   }
+
   addBook() {
     const books = JSON.parse(localStorage.getItem('books') || '[]');
     const book = {
-          title: this.title,
-          author: this.author,
+      title: this.title,
+      author: this.author,
     };
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
     return true;
   }
+
   previewBook(div, arrLength) {
     div.innerHTML += `
       <p id="${arrLength}-del" class="book-class">
@@ -21,12 +23,14 @@ class MyBooks {
           <button class="delete-button" id="${arrLength}">Remove</button>
       </p>`;
   }
+
   static removeBook(id) {
     const books = JSON.parse(localStorage.getItem('books') || '[]');
     books.splice(id, 1);
     localStorage.setItem('books', JSON.stringify(books));
     return true;
   }
+
   static displayBooks(booksDiv) {
     const books = JSON.parse(localStorage.getItem('books') || '[]');
     if (books !== null) {
